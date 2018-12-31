0b4b3b47d52aca736145e46418caf1705d1ec982
Gaganpreet <gaganpreet.arora@gmail.com>
2018-12-31 11:37:49 +0100

    Private class variables to avoid conflicts

--- petroleum/exclusive_choice.py
+++ petroleum/exclusive_choice.py
@@ -5,19 +5,19 @@ from petroleum.task import Task
 
 class ExclusiveChoice(Task):
     def __init__(self, name=None, *args, **kwargs):
-        self.conditional_tasks = []
+        self._conditional_tasks = []
         super().__init__(name=None, *args, **kwargs)
 
     def get_next_task(self, task_status):
-        for conditional_task in self.conditional_tasks:
+        for conditional_task in self._conditional_tasks:
             result = conditional_task.condition(task_status)
             if not isinstance(result, bool):
                 raise PetroleumException('Condition %s did not return bool' %
                                          conditional_task.condition)
             if result is True:
                 return conditional_task.task
-        return self.next_task
+        return self._next_task
 
     def connect_if(self, task, condition):
         conditional_task = ConditionalTask(task=task, condition=condition)
-        self.conditional_tasks.append(conditional_task)
+        self._conditional_tasks.append(conditional_task)
