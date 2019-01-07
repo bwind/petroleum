528ced23cc2bd8169c41b756b0e6afd8dbac1aed
Gaganpreet <gaganpreet.arora@gmail.com>
2019-01-07 16:14:29 +0100

    Convert to dataclass

--- petroleum/task_status.py
+++ petroleum/task_status.py
@@ -1,13 +1,18 @@
-class TaskStatus:
+from dataclasses import dataclass, field
+
+
+class TaskStatusEnum:
     WAITING = 'WAITING'
     COMPLETED = 'COMPLETED'
     FAILED = 'FAILED'
 
-    def __init__(self, status, inputs=None, outputs=None, exception=None):
-        self.status = status
-        self.inputs = inputs or {}
-        self.outputs = outputs or {}
-        self.exception = exception
+
+@dataclass
+class TaskStatus:
+    status: str
+    inputs: dict = field(default_factory=dict)
+    outputs: dict = field(default_factory=dict)
+    exception = None
 
     def __repr__(self):
         return '<%s (%s)>' % (self.__class__.__name__, self.status)
