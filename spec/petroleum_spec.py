74aa31dff96535a778b2146cea38931e009d9d4b
Gaganpreet <gaganpreet.arora@gmail.com>
2019-01-07 17:23:35 +0100

    Update test specs

--- spec/petroleum_spec.py
+++ spec/petroleum_spec.py
@@ -3,60 +3,60 @@ from mamba import before, context, description, it
 from petroleum import Task, Workflow, WorkflowStatus
 
 
-with description('task'):
-    with it('can instantiate without arguments'):
+with description("task"):
+    with it("can instantiate without arguments"):
         expect(lambda: Task()).not_to(raise_error(Exception))
 
-    with it('accepts arbitrary keyword arguments as task data'):
-        task = Task(foo='bar')
-        expect(task.foo).to(equal('bar'))
+    with it("accepts arbitrary keyword arguments as task data"):
+        task = Task(foo="bar")
+        expect(task.foo).to(equal("bar"))
 
 
-with description('workflow') as self:
+with description("workflow") as self:
     with before.each:
         self.task = Task()
-        self.workflow = Workflow(start_task=self.task)
+        self.workflow = Workflow(
+            start_task=self.task,
+            task_to_id_mapper=lambda task: "task_id",
+            id_to_task_mapper=lambda task_id: self.task,
+        )
 
-    with context('when resuming'):
-        with it('accepts arbitrary keyword arguments as inputs'):
-            self.workflow.resume(foo='bar')
+    with context("when resuming"):
+        with it("accepts arbitrary keyword arguments as inputs"):
+            self.workflow.resume(foo="bar")
 
-    with context('when task is not ready'):
+    with context("when task is not ready"):
         with before.each:
             self.task.is_ready = lambda **i: False
 
-        with it('returns suspended workflow status'):
+        with it("returns suspended workflow status"):
             workflow_status = self.workflow.start()
             expect(workflow_status.status).to(equal(WorkflowStatus.SUSPENDED))
 
-        with it('returns workflow status with task inputs'):
-            inputs = {'foo': 'bar'}
+        with it("returns workflow status with task inputs"):
+            inputs = {"foo": "bar"}
             workflow_status = self.workflow.start(**inputs)
             expect(workflow_status.inputs).to(equal(inputs))
 
 
-with description('workflow data') as self:
-    with before.each:
-        self.task = Task()
-        self.workflow = Workflow(foo='bar', start_task=self.task)
-        self.workflow.start()
+with description("json encoder"):
+    with context("task.to_json"):
+        with it("returns json"):
+            expect(Task().to_json()).to(
+                equal('{"_name": null, "_next_task": null}')
+            )
 
-    with it('has access to workflow data'):
-        expect(self.task.workflow_data['foo']).to(equal('bar'))
-
-    with it('modifies workflow data'):
-        self.task.workflow_data['foo'] = 'baz'
-        expect(self.workflow.workflow_data['foo']).to(equal('baz'))
-
-
-with description('json encoder'):
-    with context('task.to_json'):
-        with it('returns json'):
-            expect(Task().to_json()).to(equal(
-                '{"name": null, "next_task": null}'))
-
-    with context('workflow.to_json'):
-        with it('returns json'):
+    with context("workflow.to_json"):
+        with it("returns json"):
             start_task = Task()
-            expect(Workflow(start_task=start_task).to_json()).to(equal(
-                '{"current_task": {"name": null, "next_task": null}, "start_task": {"name": null, "next_task": null}, "workflow_data": {}}'))  # noqa: E501
+            expect(
+                Workflow(
+                    start_task=start_task,
+                    task_to_id_mapper=lambda task: "id",
+                    id_to_task_mapper=lambda id: start_task,
+                ).to_json()
+            ).to(
+                equal(
+                    '{"current_task": {"name": null, "next_task": null}, "start_task": {"name": null, "next_task": null}, "workflow_data": {}}'  # noqa: E501
+                )
+            )
