e90e8e1ba82f1523f8bb6e3d54926253ae263342
Gaganpreet <gaganpreet.arora@gmail.com>
2019-01-08 14:48:57 +0100

    Tests

--- spec/petroleum_spec.py
+++ spec/petroleum_spec.py
@@ -1,6 +1,13 @@
+import json
 from expects import expect, equal, raise_error
 from mamba import before, context, description, it
-from petroleum import Task, Workflow, WorkflowStatus
+from petroleum import (
+    Task,
+    Workflow,
+    WorkflowStatus,
+    ExclusiveChoice,
+    WorkflowStatus,
+)
 
 
 with description("task"):
@@ -46,8 +53,10 @@ with description("json encoder"):
                 equal('{"_name": null, "_next_task": null}')
             )
 
+
+with description("workflow state"):
     with context("workflow.get_state"):
-        with it("returns json"):
+        with it("returns state"):
             start_task = Task()
             expect(
                 Workflow(
@@ -55,8 +64,59 @@ with description("json encoder"):
                     task_to_id_mapper=lambda task: "id",
                     id_to_task_mapper=lambda id: start_task,
                 ).get_state()
-            ).to(
-                equal(
-                    {'task_log': [], 'next_task_id': 'id'}
-                )
+            ).to(equal({"task_log": [], "next_task_id": "id"}))
+
+with description("run workflow"):
+    with before.each:
+        start_task = Task()
+        next_task = ExclusiveChoice()
+        another_task = Task()
+
+        self.tasks = {
+            "start_task": start_task,
+            "next_task": next_task,
+            "another_task": another_task,
+        }
+
+        def task_to_id_mapper(task):
+            for k, v in self.tasks.items():
+                if v == task:
+                    return k
+
+        start_task.connect(next_task)
+        next_task.connect_if(another_task, lambda *args: False)
+
+        self.workflow = Workflow(
+            start_task=start_task,
+            task_to_id_mapper=task_to_id_mapper,
+            id_to_task_mapper=lambda task_id: self.tasks[task_id],
+        )
+
+    with context("workflow.get_state 2"):
+        with before.each:
+            for task in self.tasks.values():
+                task.run = lambda **args: {"success": True}
+            self.tasks["next_task"].is_ready = lambda **args: False
+        with it("returns state"):
+            status = self.workflow.start()
+
+            expect(status.status).to(equal(WorkflowStatus.SUSPENDED))
+
+            state = self.workflow.get_state()
+            expect(len(state['task_log'])).to(equal(2))
+            expect(state['next_task_id']).to(equal('next_task'))
+
+            self.tasks["next_task"].is_ready = lambda **args: True
+
+            # Create a new workflow
+            workflow = Workflow(
+                start_task=self.workflow.start_task,
+                task_to_id_mapper=self.workflow.task_to_id_mapper,
+                id_to_task_mapper=lambda task_id: self.tasks[task_id],
+                state=state,
             )
+            expect(len(state['task_log'])).to(equal(2))
+
+            status = workflow.resume()
+            expect(status.status).to(equal(WorkflowStatus.COMPLETED))
+            expect(len(state['task_log'])).to(equal(4))
