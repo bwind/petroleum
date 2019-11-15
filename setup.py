e31e12d604c1ee3d3a89981a2b003eb8be3997de
Bas Wind <mailtobwind@gmail.com>
2019-11-15 16:14:53 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="petroleum",
-    version="0.0.1",
+    version="0.0.2",
     author="Bas Wind",
     author_email="mailtobwind+petroleum@gmail.com",
     description="A pure workflow engine for Python",
